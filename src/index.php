c44f0f4979ff93d72d83bf8f0bd6c981df8b727f
Axodouble <53979495+Axodouble@users.noreply.github.com>
2024-05-09 22:39:38 +0200

    Style change of the entire website and php update.

---
+++ src/index.php
@@ -0,0 +1,76 @@
+<?php
+error_reporting(E_ALL);
+
+$quotes = array(
+    "\"<a href=\"https://www.youtube.com/watch?v=W3grGj4oUR8\"><i>I'm a silly boy kisser.</i></a>\"<a href=\"https://www.youtube.com/watch?v=W3grGj4oUR8\"><c> - Smeko.<c></a><br>",
+    "\"<a><i>If you were a worm I would shake you every day until you die of a severe brain concussion.</i></a>\" - Jazzodouble.<br>",
+    "\"<a><i>I don't like you I tolerate you.</a></i>\" - Hoodie.<br>",
+    "\"<a><i>There is a price to pay for speaking the truth. There is a bigger price for living a lie.</a></i>\" - Miku.<br>",
+    "\"<a><i>It ain't gay if it's a femboy.</i></a>\" - KaNin. (<a href=\"https://xdbl.dev/i?i=rkmLY\">Essay</a>)<br>",
+    "\"<a><i>Men don't even know what they're doing half the time- like-</i></a>\" - Jazzodouble.<br>",
+    "\"<a><i>It's getting pretty caucasian in here.</i></a>\" - <a href=\"https://metsh.tech/\"><c>Metshtival.<tiny> click me~</tiny><c></a><br>",
+    "\"<a href=\"https://www.youtube.com/watch?v=L3wKzyIN1yk\"><i>I remember that one time that Sly downloaded the Physics Mod, killed a cow and then (messed around with) it.</i></a>\" - <a href=\"https://www.youtube.com/watch?v=L3wKzyIN1yk\"><c>Ekstacy.<c></a><br>",
+    "\"<a><i>Money can't buy back your youth when you're old, a friend when you're lonely, or peace to your soul.</i></a>\" - Johnny Cash.<br>",
+    "\"<a><i>Can't shake the devil's hand and say you're only kidding.</i></a>\" - on <a href=\"https://www.youtube.com/watch?v=Ow-nuHCTA5E\">Your Racist Friend</a>, by They Might Be Giants.<br>",
+    "\"<a><i>A weapon does not decide whether or not to kill. A weapon is a manifestation of a decision that has already been made.</i></a>\" - On <a href=\"https://www.amazon.com/Cellist-Sarajevo-Steven-Galloway/dp/1594483655\">The Cellist of Sarajevo</a>, by Steven Galloway.<br>",
+    "\"<a><i>You can't call yourself peaceful if you're not capable of violence. If you're not capable of violence, you're not peaceful; you're harmless.</i></a>\" - Someone.<br>",
+    "\"<a><i>I do believe that, where there is only a choice between cowardice and violence, I would advise violence.</i></a>\" - Mahatma Gandhi.<br>",
+    "\"<a><i>Then there was the war, and I married it because there was nothing else when I reached the age of falling in love.</i></a>\" - On <a href=\"https://www.amazon.com/Forgotten-Soldier-Guy-Sajer/dp/1574882864\">The Forgotten Soldier</a>, by Guy Sajer.<br>",
+    "\"<a><i>I fear all we have done is to awaken a sleeping giant and fill him with a terrible resolve.</i></a>\" - Isoroku Yamamoto.<br>",
+    "\"<a><i>Your mind will always shutdown before your body, only way to test your full potential is to get pushed to your limits.</a></i>\" - Galah.<br>",
+    "\"<a><i>Despite all my beans, I'm still just a rat in some jeans.</i></a>\" - Jazzodouble.<br>",
+    "\"<a><i>It is what it is.</i></a>\" - ZoliWorks.<br><br><br><br>Check out <a href=\"https://bisecthosting.com/ZWS\">BisectHosting</a> if you're looking for an affordable server! Use the code ZWS at checkout for a 25% discount! ",
+    "\"<a><i>I hate the word homophobia. It's not a phobia. You're not scared. You're an asshole.</i></a>\" - Morgan Freeman.<br>",
+    "\"<a><i>If you're not in the state of post-nut clarity, you're in state of pre-nut delusion.</i></a>\" - Taki.<br>",
+    "\"<a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><i>My doctrines exist to rid maternity hospitals of their horror, to preserve the wife for her husband and the mother for her child.</i></a>\" - <a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><c>Ignaz Semmelweis.</c></a>",
+    "\"<a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><i>My doctrines exist to rid maternity hospitals of their horror, to preserve the wife for her husband and the mother for her child.</i></a>\" - <a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><c>Ignaz Semmelweis.</c></a>",
+    "\"<a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><i>My doctrines exist to rid maternity hospitals of their horror, to preserve the wife for her husband and the mother for her child.</i></a>\" - <a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><c>Ignaz Semmelweis.</c></a>",
+    "\"<a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><i>My doctrines exist to rid maternity hospitals of their horror, to preserve the wife for her husband and the mother for her child.</i></a>\" - <a href=\"https://en.wikipedia.org/wiki/Ignaz_Semmelweis\"><c>Ignaz Semmelweis.</c></a>",
+    "\"<a><i>A new scientific truth does not triumph by convincing its opponents and making them see the light, but rather because its opponents eventually die, and a new generation grows up that is familiar with it.</i></a>\" - Max Planck.<br>",
+    "\"<a><i>It is not the strongest of the species that survives, nor the most intelligent that survives. It is the one that is most adaptable to change.</i></a>\" - Charles Darwin.<br>",
+    "\"<a><i>School taught us that your skull is designed to protect your brain, you reach the streets and you realize that your brain is what you use to protect your skull.</i></a>\" - Bamodu.<br>",
+    "\"<a><i>Don't waste your time chasing butterflies. Mend your garden, and the butterflies will come.</i></a>\" - Mário Quintana.<br>",
+);
+
+function loadRandomQuote($amount)
+{
+    // Make the quotes array global
+    global $quotes;
+    // Shuffle the quotes array
+    shuffle($quotes);
+    // Select the first three quotes
+    $randomQuotes = array_slice($quotes, 0, $amount);
+    // Return the selected quotes
+    return implode("", $randomQuotes);
+}
+
+function loadHTML($message)
+{
+?>
+    <html>
+
+    <head>
+        <link rel="stylesheet" href="/style.php">
+        <meta charset="UTF-8">
+        <title>axodouble.com</title>
+    </head>
+
+    <body>
+        <div class="parent">
+            <a href="https://axodouble.com/">
+                <h1>AXODOUBLE.COM</h1>
+            </a>
+            Welcome to axodouble.com, <a href='https://xdbl.dev/?l=0eSF3'>join</a> my <a href='https://xdbl.dev/?l=0eSF3'>discord</a>!<br>
+            <?php echo ($message); ?>
+            <?php
+            echo ('<h3>Random Quote.</h3>');
+            echo (loadRandomQuote(1));
+            ?>
+        </div>
+    </body>
+
+    </html>
+<?php
+}
+
+loadHTML("");
